af81de3c3ea3662f77d77cf282a3b132a260d7f2
Rémi Puigsech <remi@iMac-de-Remi.local>
2026-05-11 16:59:49 +0200

    routes correctly match

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter> {/* <-- Enveloppe ton App */}
+    <BrowserRouter basename="/MedGlass-website/">
       <App />
     </BrowserRouter>
   </StrictMode>,
